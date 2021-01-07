95d903d05646cb80e23f86beaac972ae6161317e
izulizul12 <77089577+izulizul12@users.noreply.github.com>
2021-01-07 13:04:22 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,26 @@
+exports.help = (id, A187, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwhatsapp, youtube) => {
+	return `
+  
+✿━━━━━━━━━━━━━✿
+
+  Hi. *${id.split("@s.whatsapp.net")[0]}* 👋️
+  
+✿━━━━━━━━━━━━━✿
+
+         ───
+📆 *${tampilTanggal}*
+⏱️ *${tampilWaktu}*
+📢 Bot Aktif ; *${aktif}*
+         ───
+
+╔════════════════════
+║ *MENU BOT ${A187}*
+╠════════════════════
+║│1. _*#menu*_
+║│5. _*#info*_
+║│5. _*#donasi*_
+║│7. _*#creator*_
+╠════════════════════
+║ _*POWERED BY FADHIL ID*_
+╚════════════════════`
+}
